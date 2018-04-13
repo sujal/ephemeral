23583456e21294c4b21d6989433742caae51a5e9
Olafur Osvaldsson <osvaldsson@icelandic.net>
2018-04-13 23:18:44 +0000

    Shell script cleanup to make re-publishing easier

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
-!#/bin/bash
+#!/bin/bash
 
-go build && \
-zip /tmp/ephemeral.zip ephemeral && \
-aws --region us-east-2 lambda update-function-code --function-name ephemeralTweets --zip-file fileb:///tmp/ephemeral.zip && \
-rm /tmp/ephemeral.zip
+which go >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    go get -d ./.
+    go build -o ephemeralTweets && \
+        zip /tmp/ephemeralTweets.zip ephemeralTweets && \
+        aws --region us-east-2 lambda update-function-code --function-name ephemeralTweets --zip-file fileb:///tmp/ephemeralTweets.zip && \
+        rm -rf /tmp/ephemeralTweets.zip ./ephemeralTweets
+else
+    echo "You need to have golang installed"
+    exit 1
+fi
