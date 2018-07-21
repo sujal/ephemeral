dfdd9cea948ccec14c84bf965ca61240e11b306b
sujal shah <sujal@sujal.net>
2018-07-21 16:05:19 -0400

    allow the region to be configured in the env

--- update.sh
+++ update.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+if [ -z $AWS_REGION ]; then
+	AWS_REGION=${AWS_DEFAULT_REGION:-us-east-2}
+	echo "defaulting to $AWS_REGION"
+fi
+
 which go >/dev/null 2>&1
 if [ $? -eq 0 ]; then
     go get -d ./.
     go build -o ephemeralTweets && \
         zip /tmp/ephemeralTweets.zip ephemeralTweets && \
-        aws --region us-east-2 lambda update-function-code --function-name ephemeralTweets --zip-file fileb:///tmp/ephemeralTweets.zip && \
+        aws --region $AWS_REGION lambda update-function-code --function-name ephemeralTweets --zip-file fileb:///tmp/ephemeralTweets.zip && \
         rm -rf /tmp/ephemeralTweets.zip ./ephemeralTweets
 else
     echo "You need to have golang installed"
